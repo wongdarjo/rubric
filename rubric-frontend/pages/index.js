acce981dd7618e8045e84a0726dfb0965728e856
wongdarjo <bimaindramulya@gmail.com>
2020-05-20 10:59:39 +0700

    update style and classes

--- rubric-frontend/pages/index.js
+++ rubric-frontend/pages/index.js
@@ -8,11 +8,11 @@ const Index = (props) => {
       <Layout>
         <div className={css['container']}>
           {props.data.map(i => (
-              <div className={css['c-card']} key={i.id}>
+              <div id={i.id} className={[css['c-card'], css['c-card--hovered']].join(' ')} key={i.id}>
                 <h2 className={css['c-card__title']}>{i.title}</h2>
-                <p className={css['c-card__description']}>{i.description}</p>
-                <Link href={`/article/detail?title=${i.title}`}>
-                  <a className={css['c-link']}>Read more...</a>
+                <p className={css['c-card__description']}>{i.description}...</p>
+                <Link href={`/article/detail?id=${i.id}`}>
+                  <a className={css['c-link']}>Read more</a>
                 </Link>
               </div>
           ))}
@@ -22,15 +22,13 @@ const Index = (props) => {
   )
 }
 
-Index.getInitialProps = async function() {
+export async function getServerSideProps() {
   const res = await axios.get('http://localhost:1337/articles');
   const data = await res.data;
 
   console.log(`Show data fetched. Count: ${data.length}`);
 
-  return {
-    data: data
-  }
+  return { props: { data } }
 }
 
 export default Index;
